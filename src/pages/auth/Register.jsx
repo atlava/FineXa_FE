28e386755897be9bda80d45f26536ecba6be8eae
atlava <lenatheaatlava@gmail.com>
2026-06-04 18:06:07 +0700

    fix: use env variable for register

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -37,7 +37,7 @@ const Register = () => {
     setIsLoading(true);
 
     try {
-      const response = await fetch('http://127.0.0.1:8000/api/register', {
+      const response = await fetch(import.meta.env.VITE_API_URL + '/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
